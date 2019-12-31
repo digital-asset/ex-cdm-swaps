96ee083e0ba182dffbcf216bc3194330112adc7f
Samir Talwar <samir.talwar@digitalasset.com>
2019-12-31 13:10:17 +0100

    Run the IntelliJ formatter on LedgerClient.
    Just to catch some inconsistencies.

--- app/src/main/scala/com/digitalasset/app/LedgerClient.scala
+++ app/src/main/scala/com/digitalasset/app/LedgerClient.scala
@@ -24,14 +24,13 @@ import scala.collection.JavaConverters._
 import com.digitalasset.ledger.api.v1.testing.TimeServiceGrpc
 import com.digitalasset.ledger.api.v1.testing.TimeServiceOuterClass.{GetTimeRequest, SetTimeRequest}
 
-case class Config
-  (
-    appId: String,
-    hostIp: String,
-    hostPort: Integer,
-    maxRecordOffset: Integer,
-    useStaticTime: Boolean
-  )
+case class Config(
+  appId: String,
+  hostIp: String,
+  hostPort: Integer,
+  maxRecordOffset: Integer,
+  useStaticTime: Boolean
+)
 
 class LedgerClient(config: Config) {
   private val client = DamlLedgerClient.forHostWithLedgerIdDiscovery(config.hostIp, config.hostPort, Optional.empty())
@@ -51,7 +50,7 @@ class LedgerClient(config: Config) {
 
   // Get current ledger time
   def getTime(): Instant = {
-    if(config.useStaticTime) {
+    if (config.useStaticTime) {
       val getRequest = GetTimeRequest.newBuilder()
         .setLedgerId(ledgerId)
         .build()
@@ -64,7 +63,7 @@ class LedgerClient(config: Config) {
 
   // Set current ledger time
   def setTime(newTime: Instant): Unit = {
-    if(config.useStaticTime) {
+    if (config.useStaticTime) {
       val currentTime = getTime()
       if (currentTime.isBefore(newTime)) {
         val currentTimestamp = Timestamp.newBuilder().setSeconds(currentTime.getEpochSecond).setNanos(currentTime.getNano).build
@@ -127,12 +126,12 @@ class LedgerClient(config: Config) {
     val acsOffset = new AtomicReference[LedgerOffset](LedgerOffset.LedgerBegin.getInstance)
 
     client.getActiveContractSetClient.getActiveContracts(transactionFilter, true)
-      .blockingForEach{response =>
+      .blockingForEach { response =>
         response.getOffset.ifPresent(offset => acsOffset.set(new LedgerOffset.Absolute(offset)))
         response.getCreatedEvents.forEach(e => update(e))
       }
 
-    client.getTransactionsClient.getTransactions(acsOffset.get, transactionFilter, true).forEach{ t =>
+    client.getTransactionsClient.getTransactions(acsOffset.get, transactionFilter, true).forEach { t =>
       t.getEvents.forEach(e => update(e))
     }
   }
@@ -198,16 +197,14 @@ class LedgerClient(config: Config) {
       cos.setRecursionLimit(1000)
       val payload = DamlLf.ArchivePayload.parseFrom(cos)
       (pId, payload.getDamlLf1)
-
-  }).toMap
+    }).toMap
   }
 }
 
 object Schema {
   type Schema = Map[String, List[Field]]
 
-  case class Field
-  (
+  case class Field(
     name: String,
     cardinality: Cardinality.Cardinality,
     `type`: String,
@@ -224,7 +221,7 @@ object Schema {
   def buildSchema(moduleName: String, lfPackage: DamlLf1.Package): Option[Schema] = {
     val module = lfPackage.getModulesList.asScala.toList.find(x => getSegmentName(x.getName) == moduleName)
     module.map { m =>
-      m.getDataTypesList.asScala.toList.map{dataType =>
+      m.getDataTypesList.asScala.toList.map { dataType =>
         val name = getSegmentName(dataType.getName)
         val fields = dataType.getRecord.getFieldsList.asScala.toList.map(f => mapType(f.getField, f.getType))
         (name, fields)
@@ -234,28 +231,28 @@ object Schema {
 
   private def mapType(name: String, damlLfType: DamlLf1.Type): Schema.Field = {
     damlLfType.getPrim.getPrim.getValueDescriptor.getName match {
-      case "INT64"        => Schema.Field(name, Cardinality.ONEOF, "PrimInt64", false, false)
-      case "DECIMAL"      => Schema.Field(name, Cardinality.ONEOF, "PrimDecimal", false, false)
-      case "TEXT"         => Schema.Field(name, Cardinality.ONEOF, "PrimText", false, false)
-      case "BOOL"         => Schema.Field(name, Cardinality.ONEOF, "PrimBool", false, false)
-      case "DATE"         => Schema.Field(name, Cardinality.ONEOF, "PrimDate", false, false)
-      case "TIMESTAMP"    => Schema.Field(name, Cardinality.ONEOF, "PrimTimestamp", false, false)
-      case "PARTY"        => Schema.Field(name, Cardinality.ONEOF, "PrimParty", false, false)
-      case "CONTRACT_ID"  => Schema.Field(name, Cardinality.ONEOF, "PrimContractId", false, false)
-      case "ARROW"        => Schema.Field(name, Cardinality.ONEOF, "PrimArrow", false, false)
-      case "OPTIONAL"     =>
+      case "INT64"       => Schema.Field(name, Cardinality.ONEOF, "PrimInt64", false, false)
+      case "DECIMAL"     => Schema.Field(name, Cardinality.ONEOF, "PrimDecimal", false, false)
+      case "TEXT"        => Schema.Field(name, Cardinality.ONEOF, "PrimText", false, false)
+      case "BOOL"        => Schema.Field(name, Cardinality.ONEOF, "PrimBool", false, false)
+      case "DATE"        => Schema.Field(name, Cardinality.ONEOF, "PrimDate", false, false)
+      case "TIMESTAMP"   => Schema.Field(name, Cardinality.ONEOF, "PrimTimestamp", false, false)
+      case "PARTY"       => Schema.Field(name, Cardinality.ONEOF, "PrimParty", false, false)
+      case "CONTRACT_ID" => Schema.Field(name, Cardinality.ONEOF, "PrimContractId", false, false)
+      case "ARROW"       => Schema.Field(name, Cardinality.ONEOF, "PrimArrow", false, false)
+      case "OPTIONAL"    =>
         val subType = mapType(name, damlLfType.getPrim.getArgsList.get(0))
         if (subType.cardinality == Cardinality.ONEOF) subType.copy(cardinality = Cardinality.OPTIONAL)
         else throw new NotImplementedError("Nested optionals or lists not supported.")
-      case "LIST"         =>
+      case "LIST"        =>
         val subType = mapType(name, damlLfType.getPrim.getArgsList.get(0))
         if (subType.cardinality == Cardinality.ONEOF) subType.copy(cardinality = Cardinality.LISTOF)
         else throw new NotImplementedError("Nested optionals or lists not supported.")
-      case "UNIT"         =>
+      case "UNIT"        =>
         if (damlLfType.getCon.getArgsCount > 0) {
           val t = getSegmentName(damlLfType.getCon.getTycon.getName)
           t match {
-            case "ReferenceWithMeta" =>
+            case "ReferenceWithMeta"      =>
               val subType = mapType(name, damlLfType.getCon.getArgs(0))
               if (subType.cardinality == Cardinality.ONEOF) subType.copy(withReference = true)
               else throw new NotImplementedError("Nested FieldWithMeta types not supported.")
@@ -263,20 +260,20 @@ object Schema {
               val subType = mapType(name, damlLfType.getCon.getArgs(0))
               if (subType.cardinality == Cardinality.ONEOF) subType.copy(withReference = true)
               else throw new NotImplementedError("Nested FieldWithMeta types not supported.")
-            case "FieldWithMeta" =>
+            case "FieldWithMeta"          =>
               val subType = mapType(name, damlLfType.getCon.getArgs(0))
               if (subType.cardinality == Cardinality.ONEOF) subType.copy(withMeta = true)
               else throw new NotImplementedError("Nested FieldWithMeta types not supported.")
-            case _ => throw new NotImplementedError("Types with arguments not supported.")
+            case _                        => throw new NotImplementedError("Types with arguments not supported.")
           }
         }
         else Schema.Field(name, Cardinality.ONEOF, getSegmentName(damlLfType.getCon.getTycon.getName), false, false)
-      case other          => throw new Exception("PrimType " + other + " not supported.")
+      case other         => throw new Exception("PrimType " + other + " not supported.")
     }
   }
 
   private def getSegmentName(name: DottedName): String = {
-    val segments = 1 to name.getSegmentsCount map(i => name.getSegments(i-1))
+    val segments = 1 to name.getSegmentsCount map (i => name.getSegments(i - 1))
     segments.mkString(".")
   }
 }
